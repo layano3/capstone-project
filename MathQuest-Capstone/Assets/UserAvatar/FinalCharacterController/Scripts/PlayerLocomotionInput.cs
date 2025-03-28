d7d1d42547c01076c752024853b03c78c330b709
Youssef Mroue <mroueh.youssef10@gmail.com>
2025-03-28 02:05:54 -0400

    Adding Avatar Movement Animation

--- MathQuest-Capstone/Assets/UserAvatar/FinalCharacterController/Scripts/PlayerLocomotionInput.cs
+++ MathQuest-Capstone/Assets/UserAvatar/FinalCharacterController/Scripts/PlayerLocomotionInput.cs
@@ -9,6 +9,9 @@ namespace UserAvatar.FinalCharacterController
     [DefaultExecutionOrder(-2)]
     public class PlayerLocomotionInput : MonoBehaviour, PlayerControls.IPlayerLocomotionMapActions
     {
+        [SerializeField] private bool holdToSprint = true;
+
+        public bool SprintToggledOn {  get; private set; }
         public PlayerControls PlayerControls { get; private set; }
         public Vector2 MovementInput { get; private set; }
         public Vector2 LookInput { get; private set; }
@@ -37,6 +40,18 @@ namespace UserAvatar.FinalCharacterController
         public void OnLook(InputAction.CallbackContext context)
         {
             LookInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnToggleSprint(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                SprintToggledOn = holdToSprint || !SprintToggledOn;
+            }
+            else if (context.canceled)
+            {
+                SprintToggledOn = !holdToSprint && SprintToggledOn;
+            }
         }
     }
 }
